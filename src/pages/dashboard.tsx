ee056058c3ecd2d7a665c30b11002a8149d938f4
Alexander Lopez Vega <alex.lopez.ve@gmail.com>
2024-07-08 21:26:37 +0200

    Did stuff on landing page and started dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,78 @@
-const Dashboard = () => {
+import { useEffect, useState } from "react";
+import { Layout, Menu, Typography } from "antd/lib";
+import { UserOutlined, VideoCameraOutlined, UploadOutlined } from "@ant-design/icons";
+import http from "http";
+import "antd/dist/reset.css";
+import "./dashboard.css";
+
+const { Header, Sider, Content } = Layout;
+const { Title } = Typography;
+
+const Dashboard: React.FC = () => {
+	const [selectedMenuItem, setSelectedMenuItem] = useState<string>("1");
+
+	useEffect(() => {
+		const options = {
+			hostname: "localhost",
+			port: 7048,
+			path: "",
+			headers: {
+				"access-control-allow-origin": "*"
+			},
+			agent: false,
+		};
+
+		http.get(options, (res: http.IncomingMessage) => {
+			console.log(res);
+		});
+	});
+
+	const renderContent = () => {
+		switch (selectedMenuItem) {
+			case "1":
+				return <h1>Asset search</h1>;
+			case "2":
+				return <h1>User assets</h1>;
+			default:
+				return <h1>Default content</h1>;
+		}
+	};
+
+	const items = [
+		{
+			key: "asset-search",
+			label: "Asset Search",
+			icon: <VideoCameraOutlined />,
+			value: <>Asset Search</>,
+		},
+		{
+			key: "user-assets",
+			label: "User Assets",
+			icon: <UserOutlined />,
+			value: <>User Assets</>,
+		},
+		{
+			key: "asset-upload",
+			label: "Asset Upload",
+			icon: <UploadOutlined />,
+			value: <>Asset Upload</>,
+		},
+	];
+
 	return (
-        <>
-            <h1>Under development</h1>
-        </>
+		<Layout style={{ minHeight: "100vh" }}>
+			<Sider width={300} breakpoint="sm">
+				<Menu items={items} theme="dark" defaultSelectedKeys={["asset-search"]} onSelect={({ key }) => setSelectedMenuItem(key)}></Menu>
+			</Sider>
+
+			<Layout>
+				<Header style={{ background: "#fff", padding: 0 }} className="header-shadow">
+					<Title level={3}>Title goes here</Title>
+				</Header>
+
+				<Content style={{ marginTop: 10 }}>{renderContent()}</Content>
+			</Layout>
+		</Layout>
 	);
 };
 
--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,57 +1,105 @@
-import { Button, Card, Form, Input, Typography } from "antd";
-import type { FormProps } from "antd";
-import "../app/globals.css";
+import React from "react";
+import { Button, Card, Form, Input, Tabs, Typography } from "antd/lib";
+import { geekblue } from "@ant-design/colors";
+import type { TabsProps } from "antd";
+import "antd/dist/reset.css";
 import "./landing.css";
 
 const { Title } = Typography;
 
-type FieldType = {
-	username?: string;
-	password?: string;
-};
+const LoginForm = () => {
+	const [form] = Form.useForm();
 
-const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
-	console.log("Success:", values);
-};
+	const onFinish = (values: any) => {
+		console.log("Login success:", values);
+	};
 
-const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (errorInfo) => {
-	console.log("Failed:", errorInfo);
-};
-
-const Landing = () => {
 	return (
-		<div id="landingContainer">
-			<Card className="box-shadow">
-				<Title level={1} style={{ textAlign: "center" }}>
+		<Form form={form} name="login" onFinish={onFinish}>
+			<Form.Item name="username" rules={[{ required: true, message: "Please input your username" }]}>
+				<Input placeholder="Username"></Input>
+			</Form.Item>
+
+			<Form.Item name="password" rules={[{ required: true, message: "Please input your password" }]}>
+				<Input placeholder="Password"></Input>
+			</Form.Item>
+
+			<Form.Item>
+				<Button type="primary" htmlType="submit">
 					Login
-				</Title>
-				<Form
-					name="test"
-					labelCol={{ span: 8 }}
-					wrapperCol={{ span: 16 }}
-					style={{ maxWidth: 600 }}
-					initialValues={{ remember: true }}
-					onFinish={onFinish}
-					onFinishFailed={onFinishFailed}
-					autoComplete="off"
-				>
-					<Form.Item<FieldType> label="Username" name="username" rules={[{ required: true, message: "Please input your username!" }]}>
-						<Input />
-					</Form.Item>
+				</Button>
+			</Form.Item>
+		</Form>
+	);
+};
 
-					<Form.Item<FieldType> label="Password" name="password" rules={[{ required: true, message: "Please input your password!" }]}>
-						<Input.Password />
-					</Form.Item>
+const RegisterForm = () => {
+	const [form] = Form.useForm();
 
-					<Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-						<Button type="primary" htmlType="submit">
-							Submit
-						</Button>
-					</Form.Item>
-				</Form>
+	const onFinish = (values: any) => {
+		console.log("Register success:", values);
+	};
+
+	return (
+		<Form form={form} name="register" onFinish={onFinish}>
+			<Form.Item name="username" rules={[{ required: true, message: "Please input your username!" }]}>
+				<Input placeholder="Username" />
+			</Form.Item>
+
+			<Form.Item name="password" rules={[{ required: true, message: "Please input your password!" }]}>
+				<Input.Password placeholder="Password" />
+			</Form.Item>
+
+			<Form.Item
+				name="confirm"
+				dependencies={["password"]}
+				hasFeedback
+				rules={[
+					{ required: true, message: "Please confirm your password!" },
+					({ getFieldValue }) => ({
+						validator(_: any, value: string) {
+							if (!value || getFieldValue("password") === value) {
+								return Promise.resolve();
+							}
+							return Promise.reject(new Error("The two passwords do not match!"));
+						},
+					}),
+				]}
+			>
+				<Input.Password placeholder="Confirm Password" />
+			</Form.Item>
+
+			<Form.Item>
+				<Button type="primary" htmlType="submit">
+					Register
+				</Button>
+			</Form.Item>
+		</Form>
+	);
+};
+
+const LandingPage: React.FC = () => {
+	const items: TabsProps["items"] = [
+		{
+			key: "login",
+			label: "Login",
+			children: <LoginForm />,
+		},
+		{
+			key: "register",
+			label: "Register",
+			children: <RegisterForm />,
+		},
+	];
+
+	return (
+		<div className="landing-page" style={{backgroundColor: geekblue[0]}}>
+			<Card className="card-shadow">
+				<Title level={2}>Rock samples app</Title>
+				<Tabs defaultActiveKey="login" items={items}></Tabs>
 			</Card>
 		</div>
 	);
 };
 
-export default Landing;
+export default LandingPage;
