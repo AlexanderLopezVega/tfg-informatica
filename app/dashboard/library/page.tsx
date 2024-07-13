3ac4d7987b5e4a7c92cfa820f85e6af9c9f2c31c
Alexander Lopez Vega <alex.lopez.ve@gmail.com>
2024-07-13 16:02:38 +0200

    [dirty]: Added library and upload pages

---
+++ app/dashboard/library/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Library: React.FC = () => {
+    return (
+        <>
+        <p>Library</p>
+        </>
+    );
+};
+
+export default Library;
---
+++ app/dashboard/upload/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Upload: React.FC = () => {
+	return (
+		<>
+			<p>Upload</p>
+		</>
+	);
+};
+
+export default Upload;
